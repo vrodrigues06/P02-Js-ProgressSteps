e51ff05f1cd8bf57fc01351488982bf90b265beb
V.Rodrigues <vitorrodrigues06@outlook.com>
2023-05-26 21:06:12 -0300

    other-way-to-do

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ let progress = progressBar.style.width;
 function initProgressBar() {
   stepOne.classList.add("active");
   function stepProgress() {
-    if (steps <= 3) {
+    if (steps <= allSteps.length) {
       allSteps[steps].classList.add("active");
       steps++;
       progress = progressBar.style.width = ((steps - 1) * 30).toString() + "%";
     }
-    if (steps === 2) {
+    if (steps > 1) {
       btnPrev.removeAttribute("disabled");
     }
-    if (steps === 4) {
+    if (steps === allSteps.length) {
       btnNext.setAttribute("disabled", "");
     }
   }
   function stepBack() {
-    if (steps <= 4) {
+    if (steps <= allSteps.length) {
       allSteps[steps - 1].classList.remove("active");
       steps--;
       progress = progressBar.style.width = ((steps - 1) * 30).toString() + "%";
@@ -30,7 +30,7 @@ function initProgressBar() {
     if (steps === 1) {
       btnPrev.setAttribute("disabled", "");
     }
-    if (steps < 4) {
+    if (steps < allSteps.length) {
       btnNext.removeAttribute("disabled");
     }
   }
@@ -39,3 +39,55 @@ function initProgressBar() {
 }
 
 initProgressBar();
+
+// !!!!Logic use by the Course (above is my own logic to reach the same goal)!!!!!
+
+// const progress = document.getElementById('progress')
+// const prev = document.getElementById('prev')
+// const next = document.getElementById('next')
+// const circles = document.querySelectorAll('.circle')
+
+// let currentActive = 1
+
+// next.addEventListener('click', () => {
+//     currentActive++
+
+//     if(currentActive > circles.length) {
+//         currentActive = circles.length
+//     }
+
+//     update()
+// })
+
+// prev.addEventListener('click', () => {
+//     currentActive--
+
+//     if(currentActive < 1) {
+//         currentActive = 1
+//     }
+
+//     update()
+// })
+
+// function update() {
+//     circles.forEach((circle, idx) => {
+//         if(idx < currentActive) {
+//             circle.classList.add('active')
+//         } else {
+//             circle.classList.remove('active')
+//         }
+//     })
+
+//     const actives = document.querySelectorAll('.active')
+
+//     progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+
+//     if(currentActive === 1) {
+//         prev.disabled = true
+//     } else if(currentActive === circles.length) {
+//         next.disabled = true
+//     } else {
+//         prev.disabled = false
+//         next.disabled = false
+//     }
+// }
